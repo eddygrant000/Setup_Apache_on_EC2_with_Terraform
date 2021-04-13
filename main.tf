efcfbec952a256412bb81a0b62768ac048dd59f0
Sachin Saini <sachin.saini@ap2v.com>
2021-04-14 00:00:16 +0530

    add http rule in group

--- main.tf
+++ main.tf
@@ -1,22 +1,28 @@
-resource "aws_security_group" "webserver" {
-  ingress_with_cidr_blocks = [
-      {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        description = "HTTP"
-        cidr_blocks = "0.0.0.0/0"
-      }
-    ]
+resource "aws_security_group" "web-sg" {
+  name = "webserver"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_instance" "MyFirstInstance" {
+
+resource "aws_instance" "WebServer" {
     ami = "ami-0cda377a1b884a1bc"
     instance_type = "t2.micro"
     tags = {
-      Name = "EC2_instance_Terraform"
+      Name = "WebServer"
     }
     key_name = "devops"
-    user_data = "${file("install_ansible.sh")}" 
+    user_data = file("install_ansible.sh")
     security_groups = [ "webserver" ]
 }
