73f54ed6b28ac810da63a1d5672b9b200946de77
Sachin Saini <sachin.saini@ap2v.com>
2021-04-13 23:37:34 +0530

    add security group and ansible setup

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+sudo apt-get update
 sudo apt install ansible -y
 git clone https://github.com/eddygrant000/Setup_Apache_on_EC2_with_Terraform.git
+sudo sed -i '/^#inventory.*  /s/^#//' /etc/ansible/ansible.cfg
+sudo echo "localhost ansible_connection=local" >> /etc/ansbile/hosts
+ansible-playbook Setup_Apache_on_EC2_with_Terraform/apache.yml 
--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+resource "aws_security_group" "webserver" {
+  ingress_with_cidr_blocks = [
+      {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        description = "HTTP"
+        cidr_blocks = "0.0.0.0/0"
+      }
+    ]
+}
+
 resource "aws_instance" "MyFirstInstance" {
     ami = "ami-0cda377a1b884a1bc"
     instance_type = "t2.micro"
@@ -6,4 +18,5 @@ resource "aws_instance" "MyFirstInstance" {
     }
     key_name = "devops"
     user_data = "${file("install_ansible.sh")}" 
+    security_groups = [ "webserver" ]
 }
